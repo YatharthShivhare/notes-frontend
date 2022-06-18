1de939f31b3f9d2595c8e87446ef096176b57d84
YatharthShivhare <yatharthshivhare@gmail.com>
2022-06-19 02:11:25 +0530

    adds header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./css/Header.css"
+
+function Header() {
+    return (
+        <div className="Header">
+            <header>
+                <h1>
+                    Notes
+                </h1>
+            </header>
+
+        </div>
+    );
+
+}
+export default Header;
